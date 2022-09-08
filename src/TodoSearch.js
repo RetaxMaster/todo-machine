e9de615989c7db0b43dec91d3adec69724bb7324
RetaxMaster <retaxmaster@hotmail.com>
2022-09-07 19:17:51 -0500

    Adicion de eventos y estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,20 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const onSearchValueChange = (event) => {
+    setSearchValue(event.target.value);
+  }
+
   return (
-    <input className="TodoSearch" placeholder="Cebolla" />
+    <input 
+      className="TodoSearch" 
+      placeholder="Cebolla"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />
   );
 }
 
